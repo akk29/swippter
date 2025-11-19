631788c6a2fb83549f1481a9f7039b482f31c38c
Akshay Kathpal <21139206+akk29@users.noreply.github.com>
2025-11-19 15:57:04 +0530

    feat: ✨ adding docker support 🐳
    1. adding docker support for backend services.
    2. adding static file directory
    3. adding gunicorn application server for application run time

--- backend/swippter/swippter/settings.py
+++ backend/swippter/swippter/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from corsheaders.defaults import default_methods
 from decouple import config
 from pathlib import Path
@@ -147,7 +148,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
