98d36ce43930e40999da33b66a6f6466361991a3
Akshay Kathpal <21139206+akk29@users.noreply.github.com>
2025-12-02 20:48:19 +0530

    feat: ✨ added mysql docker support
    1. moving dockerfile command from run to entrypoint script
    2. adding env config for mysql container

---
+++ infra/docker/backend/server/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+python manage.py makemigrations app
+python manage.py migrate app
+python manage.py makemigrations
+python manage.py migrate
+python manage.py create_admin
+python manage.py collectstatic --no-input
+gunicorn swippter.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
