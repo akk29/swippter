d225ec7c172852d7743e1e4949e906679f8c3297
Akshay Kathpal <21139206+akk29@users.noreply.github.com>
2026-01-05 17:16:40 +0530

    refactor: ♻️ refactoring code
    1. added brypt authentication password checking utility to direct check the password hash and user password provided in API

--- backend/swippter/app/dao/user_dao.py
+++ backend/swippter/app/dao/user_dao.py
@@ -2,7 +2,7 @@ import bcrypt
 from app.core.exceptions import UnauthorizedError
 from app.dao.base_dao import BaseDAO
 from app.models.user import User
-from app.utils.utilities import F, generate_password_hash
+from app.utils.utilities import F
 
 class UserDAO(BaseDAO):
 
@@ -13,12 +13,10 @@ class UserDAO(BaseDAO):
         self.model = User
 
     def authenticate_user(self, *args, **kwargs):
-        user = self.fetch_one(raise_error=True, **{F.EMAIL: kwargs[F.EMAIL]})        
-        hashed = generate_password_hash(kwargs[F.PASSWORD], user.salt)
-        # bypass
-        # correct implementation
-        # if bcrypt.checkpw(user.password.encode(F.UTF8),hashed.encode(F.UTF8)):
-        if(user.password.encode(F.UTF8)==hashed.encode(F.UTF8)):
+        user = self.fetch_one(raise_error=True, **{F.EMAIL: kwargs[F.EMAIL]})
+        if bcrypt.checkpw(
+            kwargs[F.PASSWORD].encode(F.UTF8), user.password.encode(F.UTF8)
+        ):
             return user
         else:
             raise UnauthorizedError(msg=F.INCORRECT_CREDENTIALS)
