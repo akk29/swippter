b7ebadc5300df7acf623a0338eb0ff2e172f4a09
Akshay Kathpal <21139206+akk29@users.noreply.github.com>
2026-01-31 21:49:20 +0530

    wip: 🚧

--- infra/docker/server/entrypoint.sh
+++ infra/docker/server/entrypoint.sh
@@ -8,4 +8,3 @@ python manage.py create_admin
 python manage.py collectstatic --no-input
 export DJANGO_SETTINGS_MODULE=swippter.settings
 opentelemetry-instrument gunicorn swippter.wsgi:application --bind 0.0.0.0:8000 --workers 4 --threads 4 --preload
-# gunicorn swippter.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
