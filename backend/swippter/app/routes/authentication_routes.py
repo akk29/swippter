b7d8b55c66898bd8e0d7210c5d7197f51b107754
Akshay Kathpal <21139206+akk29@users.noreply.github.com>
2026-03-03 14:02:33 +0530

    wip: 🚧

--- backend/swippter/app/routes/authentication_routes.py
+++ backend/swippter/app/routes/authentication_routes.py
@@ -1,17 +1,17 @@
 from django.urls import path
 from app.views.authentication_views import SignupView, SigninView, ForgotView, VerifyTokenView, ChangePasswordView
 from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-    TokenVerifyView,
-    TokenBlacklistView,
+    TokenRefreshView as RF_TokenRefreshView,
+    TokenVerifyView as RF_TokenVerifyView,
+    TokenBlacklistView as RF_TokenBlacklistView,
 )
 
 urlpatterns = [
     path("api/v1/signup", SignupView.as_view(), name="signup"),
     path("api/v1/signin", SigninView.as_view(), name="signin"),
-    path("api/v1/signout", TokenBlacklistView.as_view(), name="signout"),
-    path("api/v1/refresh", TokenRefreshView.as_view(), name="refresh"),
-    path("api/v1/verify", TokenVerifyView.as_view(), name="verify"),
+    path("api/v1/signout", RF_TokenBlacklistView.as_view(), name="signout"),
+    path("api/v1/refresh", RF_TokenRefreshView.as_view(), name="refresh"),
+    path("api/v1/verify", RF_TokenVerifyView.as_view(), name="verify"),
     path("api/v1/forgot", ForgotView.as_view(), name="forgot"),
     path("api/v1/verify-token/<uidb64>/<token>", VerifyTokenView.as_view(), name="verify-token"),
     path("api/v1/change-password", ChangePasswordView.as_view(), name="change-password"),
